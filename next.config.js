399085e6103ce307109c959cb36fbdf6442af3e7
Gabriel Rodrigues <grodrigues.ti@outlook.com>
2022-09-16 19:16:18 -0300

    implementing redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async() => {
+    return [{
+      source: '/dashboard',
+      destination: '/pokemons',
+      permanent: false
+    }]
+  }
 }
 
 module.exports = nextConfig
