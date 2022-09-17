7f97a41af3bf22dedf9accc4c8c5086cb354734a
Gabriel Rodrigues <grodrigues.ti@outlook.com>
2022-09-17 10:17:57 -0300

    working with environment variables

--- pages/news/index.js
+++ pages/news/index.js
@@ -2,11 +2,15 @@ function NewsArticleList({ data }){
     return (
         <>
             <h1 className="content">{data}</h1>
+            <p>{process.env.NEXT_PUBLIC_ID}</p>
         </>
     )
 }
 
 export async function getServerSideProps(context) {
+    const user = process.env.DB_USER
+    const password = process.env.DB_PASSWORD
+    console.log(user, password)
     return {
         props: {
             data: context.preview ? 'List of draft Articles': 'List of published Articles'
